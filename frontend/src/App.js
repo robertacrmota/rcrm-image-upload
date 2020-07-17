56c625577d711574aef81453ca92fda0507ef320
Roberta Mota <roberta.cabralmota@ucalgary.ca>
2020-07-17 17:21:59 -0400

    add feature for image conversion - from arrayBuffer to Base64

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,13 +4,15 @@ import axios from 'axios';
 function App() {
   const [avatar, setAvatar] = useState('');
 
-  const handleSubmit = evt => {
-    evt.preventDefault();
-    uploadImage();
+  const handleSubmit = file => {
+  
+    const encodeImage = (mimetype, arrayBuffer) => {
+        let u8 = new Uint8Array(arrayBuffer)
+        const b64encoded = btoa([].reduce.call(new Uint8Array(arrayBuffer),function(p,c){return p+String.fromCharCode(c)},''))
+        return "data:"+mimetype+";base64,"+b64encoded;
+    }
 
-    async function uploadImage() {
-      const file = evt.target.files[0];
-
+    const uploadImage = async () => {
       const data = new FormData();
       data.append('file', file);
       data.append('filename', file.name);
@@ -20,15 +22,20 @@ function App() {
                                         headers: { 'Content-Type': 'multipart/form-data'}
       });
 
-      console.log(result);
+      // console.log(result);
+      const dataURL = encodeImage(result.data.mimetype, result.data.buffer.data);
+      // console.log(dataURL);
+      setAvatar(dataURL);
     }
+
+    uploadImage();
   };
 
   return (
     <div>
       <h1>Upload an image to the server</h1>
       <form encType="multipart/form-data"> 
-              <input type="file" name="avatar" onChange={handleSubmit}/>
+              <input type="file" name="avatar" onChange={e => handleSubmit(e.target.files[0])}/>
       </form>
       {avatar ? <img src={avatar} width="400" height="300"/> : undefined}
     </div>
